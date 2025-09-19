8f71af9fcfdc472f5c08ad8748c555910d4397d5
Jose Campos <130111367+suportecnico@users.noreply.github.com>
2025-09-19 19:26:17 -0300

    atualização...

--- curso.js
+++ curso.js
@@ -20,11 +20,13 @@ function calculoDeCerca() {// Aqui ao invés de passar o parametro dentro da fun
 
 
     let resultadoHtml = `
-    <p>Arame: ${Math.ceil (arame)} metros!</P>
-    <p>Hastes Esticadoras: ${Math.ceil (hasteEsticadora)} unidades!</p>
-    <p>Hastes Passadoras: ${Math.ceil (hastePassadora)} unidades!</p>
-    <p>Isoladores Tipo Castanha: ${Math.ceil (isoladoresTipoCastanha)} unidades!</p>
-    <p>Ganchos e Porcas: ${Math.ceil(ganchosPorcas)} unidades!</p>
+    <h4>Lembre-se, esse é um valor base, pois cada perímetro tem sua especifidade!</h4>
+    <p><strong>Arame:</strong> ${Math.ceil (arame)} metros!</P>
+    <p><strong>Hastes Esticadoras:</strong> ${Math.ceil (hasteEsticadora)} unidades!</p>
+    <p><strong>Hastes Passadoras:</strong> ${Math.ceil (hastePassadora)} unidades!</p>
+    <p><strong>Isoladores Tipo Castanha:</strong> ${Math.ceil (isoladoresTipoCastanha)} unidades!</p>
+    <p><strong>Ganchos e Porcas: </strong>${Math.ceil(ganchosPorcas)} unidades!</p>
+
     `;
 
     document.getElementById("resultado").innerHTML = resultadoHtml;
@@ -32,6 +34,6 @@ function calculoDeCerca() {// Aqui ao invés de passar o parametro dentro da fun
 
 function limparCalculo() {
     if(calculoDeCerca !== 0) {
-        document.getElementById("resultado").innerHTML = "Conteúdo";
+        document.getElementById("resultado").innerHTML = "<p>Preencha acima &#X2b06; com os metros do perímetro onde será instalada a cerca elétrica!</p>";
     }
 }
